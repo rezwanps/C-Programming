90793c01af4ec3ddfacc578997067375b7931e9f
Rezwan Mahmud <rezwan.mahmud.007@gmail.com>
2026-04-22 14:21:53 +0600

    Delete Module-2 directory

--- Module-2/contest-1-03.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int input;
-    scanf("%d", &input);
-    printf("%d", input + 5);
-    return 0;
-}
--- Module-2/increment.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main(){
-    int a = 50;
-    printf("%d\n", a++);
-    printf("%d", a);
-    return 0;
-}
